d206d561aa6844f2c462db3aceeb4148705f412e
Daniele Romeo <danieleromeo.ele@gmail.com>
2025-06-22 11:04:55 +0200

    Fix

--- bot.py
+++ bot.py
@@ -1,20 +1,13 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
-import sys
+from telegram.ext import ApplicationBuilder, CommandHandler
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-
-if not TOKEN:
-    print("Errore: la variabile TELEGRAM_TOKEN non è impostata.", file=sys.stderr)
-    sys.exit(1)
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Ciao! Sono un bot Telegram su Render!")
+TOKEN = os.environ.get("TELEGRAM_TOKEN")
 
 app = ApplicationBuilder().token(TOKEN).build()
+
+async def start(update, context):
+    await update.message.reply_text("Ciao! Sono un bot Railway 🚂")
+
 app.add_handler(CommandHandler("start", start))
 
-if __name__ == "__main__":
-    print("Bot in esecuzione...")
-    app.run_polling()
+app.run_polling()
